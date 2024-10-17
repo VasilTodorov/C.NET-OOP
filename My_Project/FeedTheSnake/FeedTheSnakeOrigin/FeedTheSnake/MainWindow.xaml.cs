2249f93f07642965d849a5132f1c909d0f9859b0
VasilTodorov <vtodorov00@gmail.com>
2024-10-17 14:48:29 +0300

    second commit

---
+++ My_Project/FeedTheSnake/FeedTheSnakeOrigin/FeedTheSnake/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FeedTheSnake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DrawSnake snake;
+        private bool susspendGame = true;
+        DrawFoodFarm foodFarm;
+        //Snake snake;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            snake = new DrawSnake(paintCanvas);
+            foodFarm = new DrawFoodFarm(paintCanvas);
+            foodFarm.Start();
+        }
+
+        private void paintCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            snake.Length++;
+            susspendGame = !susspendGame;
+            Point a = new Point(1, 1);
+            Point b = new Point(2, 2);
+            txtAnswer.Text = "is biting: No , Check: " + a.Distance(b);
+        }
+
+        private void paintCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!susspendGame)
+            {
+                Point destination = e.GetPosition(paintCanvas);
+                snake.Move(destination);
+                txtAnswer.Text = $"distanse: {snake.TailDis:F6}";
+                if (snake.Suspended)
+                {
+                    susspendGame = true;
+                    txtAnswer.Text = $"is biting: Yes + distanseN: {snake.TailDis:F6}";
+                }
+                else
+                {
+                    txtAnswer.Text = $"distanse: {snake.TailDis:F6}";
+                }
+                if (foodFarm.IsNearbyFoodEaten(destination))
+                {
+                    snake.Length++;
+                }
+            }
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void paintCanvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //snake.DefaultSnake();
+            //susspendGame = true;
+        }
+    }
+}
---
+++ My_Project/FeedTheSnake/FeedTheSnakeOrigin/FeedTheSnake/Snake.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FeedTheSnake
+{
+    /// <summary>
+    /// class DrawSnake illustrates the drawing
+    /// and fuctionality of the snake in the game
+    /// feed the snake
+    /// </summary>
+    public class DrawSnake
+    {
+        private const double SEGMENT = 20;//TODO can make it const
+        private bool susspended = true;
+
+        private Canvas paintCanvas;
+        private Polyline body;
+        private Ellipse head;
+
+        private int length;
+		
+
+        /// <summary>
+        /// Default constructor 
+        /// </summary>
+        /// <param name="paintCanvas"></param>
+        public DrawSnake(Canvas paintCanvas)
+        {          
+            //init variables
+            this.paintCanvas = paintCanvas;            
+            body = new Polyline();
+            head = new Ellipse();
+        
+            head.MouseLeftButtonDown += (x, y) => { susspended = false; };
+
+            DefaultSnake();
+         
+            paintCanvas.Children.Add(body);
+            paintCanvas.Children.Add(head);
+        }
+      
+        //public void HeadSelection(MouseButtonEventHandler a)
+        //{
+        //    head.MouseLeftButtonDown += a;
+        //}
+        /// <summary>
+        /// The length of the snake cant never be less than 3
+        /// </summary>
+        public int Length 
+        {
+            get => length;
+            set => length = value >= 3 ? value : 3 ; 
+        }
+
+        public bool Suspended { get => susspended; }
+
+        public double TailDis 
+        { 
+            get
+            {
+                //the way I set head pont isnt the center but up left corner.
+                Point headPoint = new Point(Canvas.GetLeft(head), Canvas.GetTop(head));
+                return body.Points[0].Distance(headPoint);
+            }
+        }
+        /// <summary>
+        /// Moves DrawSnake to a given point
+        /// </summary>
+        /// <param name="goal"></param>
+        public void Move(Point goal)
+        {            
+            if (susspended) { return; }           
+            
+            //the end of the snake body weher the head will be
+            Point headBodyPoint = body.Points.Last();           
+
+            //the snake moves only one SEGMENT at a time
+            //by adding point at the end and removing at the beginig of body
+            while (headBodyPoint.Distance(goal) > SEGMENT)
+            {              
+                //the added new point is at a distance "SEGMENT" from "headBodyPoint"
+                //towards goal point
+                headBodyPoint = headBodyPoint.MoveTowards(goal, SEGMENT);
+                body.Points.Add(headBodyPoint);
+
+                Canvas.SetTop(head, headBodyPoint.Y - head.Height / 2);
+                Canvas.SetLeft(head, headBodyPoint.X - head.Width / 2);
+
+                //removes points from snake tail untill length == body.Points.Count
+                //this way we show the new lemgth of the snake
+                while (body.Points.Count > length)
+                { body.Points.RemoveAt(0); }
+
+                //And repeat untill the distance is less than SEGMENT
+               if(!isValidMove())
+                {
+                    susspended = true;
+                    return;
+                }
+            }
+
+            //Canvas.SetTop(head, goal.Y - head.Height / 2);
+            //Canvas.SetLeft(head, goal.X - head.Width / 2);
+            //if (!isValidMove())
+            //{
+            //    susspended = true;
+            //    //break;
+            //}
+        }
+
+        public bool isValidMove()
+        {
+            return !(this.IsEatingItself() || this.isHittingTheWall());
+        }
+
+        private bool IsEatingItself()
+        {
+            //eating itself moves 
+            //back move
+            //body eat
+            //tail eat
+
+
+            Point headPoint = new Point(Canvas.GetLeft(head), Canvas.GetTop(head));
+            //Point headPoint = body.Points.Last();
+            ////Bug when it its
+            ///
+            // body
+            //for (int i = 1; i < body.Points.Count - 3; i++)
+            //{
+            //    if (body.Points[i].Distance(headPoint) < SEGMENT*3/4)
+            //        return true;
+            //}
+
+            //tail 
+            if (body.Points[0].Distance(headPoint) <= SEGMENT*0.75)
+                return true;
+            return false;
+        }
+
+        
+        private bool isHittingTheWall() {
+            return false;
+        }
+        /// <summary>
+        /// Sets DrawSnake body and head default values
+        /// also the length of body
+        /// </summary>
+        public void DefaultSnake()
+        {
+            //clears old body
+            body.Points.Clear();
+
+            //sets default cordinates for body
+            body.Points.Add(new Point(0, 0 + 40));
+            body.Points.Add(new Point(SEGMENT, SEGMENT + 40));
+            body.Points.Add(new Point(SEGMENT * 2, 0 + 40));
+            body.Points.Add(new Point(SEGMENT * 3, SEGMENT + 40));
+            body.Points.Add(new Point(SEGMENT * 4, SEGMENT + 40));
+            body.Points.Add(new Point(SEGMENT * 5, SEGMENT + 40));
+            body.Points.Add(new Point(SEGMENT * 6, SEGMENT + 40));
+
+            //sets field length of body new value
+            length = body.Points.Count;
+
+            //sets default properties for body 
+            body.Stroke = Brushes.Green;          
+            body.StrokeThickness = SEGMENT;
+            //body.StrokeStartLineCap = PenLineCap.Round;
+            //body.StrokeEndLineCap = PenLineCap.Round;
+            body.StrokeLineJoin = PenLineJoin.Round;
+
+            //sets default properties for head           
+            head.Width = SEGMENT * 2;
+            head.Height = SEGMENT * 2;
+            head.Fill = Brushes.Green;
+            //head.Fill = Brushes.Transparent;
+
+            //sets head position
+            Point headPoint = body.Points.Last();
+            Canvas.SetTop(head, headPoint.Y - head.Height / 2);
+            Canvas.SetLeft(head, headPoint.X - head.Width / 2);
+
+        }
+    }
+}
